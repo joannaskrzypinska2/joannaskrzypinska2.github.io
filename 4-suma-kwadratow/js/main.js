0aed89621112fc622c9efbbf027b70a26c9e19c6
jskrzypinska <joannaskrzypinska2@gmail.com>
2020-06-27 13:06:39 +0300

    suma-kwadratow-js

---
+++ 4-suma-kwadratow/js/main.js
@@ -0,0 +1,22 @@
+SumaKwadratów([0,1,2,3,4,5]);
+function SumaKwadratów(parametr) {
+    /*  zapisanie tablicy jako parametr funkcji */
+    let tablica = [];
+    tablica = parametr;
+
+    /*  utworzenie zmiennej do sumowania */
+    let suma = 0;
+
+    /* pętla sumująca i mnożąca kolejne elementy tablicy */
+    for (let i = 0; i < tablica.length; i++) {
+       suma =  suma + Math.pow(tablica[i], 2) ;
+     }
+    
+    /* wyswietlenie wyniku  - musi być przed return */
+    
+    console.log("Suma kwadratów liczb w tablicy wynosi: " + suma + "."); 
+     
+    /* zwrócenie wyniku działania funkcji */
+    return suma; 
+}
+
