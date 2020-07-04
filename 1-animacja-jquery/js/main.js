014eb244f48e5babfa6d51eeef352ae9c01af03d
jskrzypinska <joannaskrzypinska2@gmail.com>
2020-07-04 11:35:53 +0300

    2 commit

--- 1-animacja-jquery/js/main.js
+++ 1-animacja-jquery/js/main.js
@@ -2,9 +2,8 @@ $(document).ready(function () {
     const square = $('div');
     $('#btn').click(function () {
         square.animate({ 'margin-left': '100px', height: '100px', width: '100px' }, 3000, function () {
-            $(this).css({ backgroundColor: 'blue', "transition": '3s' }), 2000, setTimeOut(function () {
-                $(this).after('<p>Animacja zakończona</p>')
-            }, 3000);
+            $(this).css({ backgroundColor: 'blue', "transition": '3s' })
         })
+        setTimeout(function () { $(square).append('<p>Animacja zakończona</p>') }, 5000)
     })
 });
