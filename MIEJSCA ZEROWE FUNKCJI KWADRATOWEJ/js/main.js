b3a41e4594cb33a4699d618e7b6af8ab817362af
jskrzypinska <joannaskrzypinska2@gmail.com>
2020-07-11 11:37:27 +0300

    1 commit

---
+++ MIEJSCA ZEROWE FUNKCJI KWADRATOWEJ/js/main.js
@@ -0,0 +1,15 @@
+const count = (a, b, c) => {
+    let result = Math.pow(b, 2)-4*a*c
+    console.log(result);
+    if (result < 0) {
+        console.log("brak miejsc zerowych");
+    } else if (result = 0) {
+        console.log("jedno miejsce zerowe");
+    } else if (result > 0) {
+        console.log("dwa miejsca zerowe");
+    }
+}
+
+count(2, -4, 2);
+count(-1, 3, 4);
+console.log(count);
