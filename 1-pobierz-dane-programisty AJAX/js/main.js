8a1585c82c86e2b722b29ea8aab6c7eaf1be544e
jskrzypinska <joannaskrzypinska2@gmail.com>
2020-07-12 15:16:06 +0300

    2 commit

--- 1-pobierz-dane-programisty AJAX/js/main.js
+++ 1-pobierz-dane-programisty AJAX/js/main.js
@@ -3,9 +3,9 @@ console.log('jQuery - $.getJSON()');
 $('#download').click(function () {
     $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
         console.log(data);
-        $('#download').after($('</div>', {
+        $('#download').after($('<div>', {
             id: 'dane-programisty'
             }));
-        $("#dane-programisty").append("<br>Imię: " + data.imie, " < br > Nazwisko: " + data.nazwisko, " < br > Zawód: " + data.zawod, " < br > Firma: " + data.firma);
+        $("#dane-programisty").append("<br>Imię: " + data.imie, "<br>Nazwisko: " + data.nazwisko, "<br> Zawód: " + data.zawod, "<br>Firma: " + data.firma);
 });
 });
